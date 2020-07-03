038ca6268987e935832ac801c439392463f05c55
tanruixuan <ruixuantan@yahoo.com.sg>
2020-07-03 14:06:44 +0800

    changed production settings

--- instance/settings.py
+++ instance/settings.py
@@ -11,3 +11,4 @@ db_uri = 'postgresql://{0}:{1}@postgres:5432/{2}'.format('fourparts',
                                                          'fourpartspassword',
                                                          'fourparts')
 SQLALCHEMY_DATABASE_URI = db_uri
+SQLALCHEMY_TRACK_MODIFICATIONS = False
