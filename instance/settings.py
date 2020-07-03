16d04d1ec91911b01be06bc0c7f3944bd2b94890
tanruixuan <ruixuantan@yahoo.com.sg>
2020-07-03 13:13:32 +0800

    production settings configured

---
+++ instance/settings.py
@@ -0,0 +1,13 @@
+DEBUG = False
+
+SERVER_NAME = 'fourparts.herokuapp.com'
+SECRET_KEY = 'insecurekeydev'
+
+# USER
+# PASSWORD
+# DB
+
+db_uri = 'postgresql://{0}:{1}@postgres:5432/{2}'.format('fourparts',
+                                                         'fourpartspassword',
+                                                         'fourparts')
+SQLALCHEMY_DATABASE_URI = db_uri
