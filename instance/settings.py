d2f413b2392f410f41716b4135d801cb33cabb02
tanruixuan <ruixuantan@yahoo.com.sg>
2020-07-17 15:01:33 +0800

    removed api key

--- instance/settings.py
+++ instance/settings.py
@@ -10,7 +10,7 @@ RESULTSTORE_PATH = 'fourpartsweb/storage/results/'
 # PASSWORD
 # DB
 
-db_uri = 'postgres://pddovxouxdhfts:8fc7ca66a729d2fb2f58efe2b2d8e83abe055ec5be827edc0677983905f879e0@ec2-54-159-138-67.compute-1.amazonaws.com:5432/dc3cihr5l0c5ao'
+db_uri = 'insert heroku key here'
 
 SQLALCHEMY_DATABASE_URI = db_uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
