b94641f7c66fbd9b5ccc40a32b78117c571bdb59
tanruixuan <ruixuantan@yahoo.com.sg>
2020-07-21 19:37:52 +0800

    Added link to download sample

--- fourpartsweb/blueprints/download/views.py
+++ fourpartsweb/blueprints/download/views.py
@@ -8,3 +8,13 @@ download = Blueprint('download', __name__, template_folder='templates')
 @download.route('/download/')
 def download_all():
     return render_template('download/download_page.html')
+
+
+@download.route('/download/sample/')
+def download_sample():
+    try: 
+        return send_from_directory('static/sample/',
+                                   'chorale_F.zip', 
+                                   as_attachment=True)
+    except:
+        return jsonify({'error': 'sample files not available'}), 500
